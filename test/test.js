2ee2764970fd725efc6805888a262d99b8f4034c
Louis Ameline <evangun2001@yahoo.fr>
2019-06-25 20:26:15 +0200

    clear

--- test/test.js
+++ test/test.js
@@ -20,39 +20,31 @@ test.on('e1.ns1.ns2', (x, y, z) => { console.log('A', x, y, z) })
 // will log A X Y Z B C D E F C G
 // G will be logged only here
 emit()
-console.log('')
 
 test.off('e2.ns1') // doesn't match any listener
 // will log A X Y Z B C D E F C
 emit()
-console.log('')
 
 test.off('e2.ns4') // matches 1 listener
 // will log A X Y Z B C D E F
 emit()
-console.log('')
 
 test.off('e1', () => {}) // doesn't match any listener
 // will log A X Y Z B C D E F
 emit()
-console.log('')
 
 test.off('e1.ns1.ns2') // matches the first listener
 // will log B C D E F
 emit()
-console.log('')
 
 test.off('.ns2') // matches 2 listeners
 // will log C D F
 emit()
-console.log('')
 
 test.off('e1') // matches 2 listeners
 // will log F
 emit()
-console.log('The end')
 
 test.off() // matches the remaining listener
 // will log nothing
 emit()
-console.log('The real end')
